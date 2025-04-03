b0ed5188d27ad5c94d2073d85366ed4272345c50
gabriel <gabriel.domiciano@aluno.ifsp.edu.br>
2025-04-03 18:19:02 -0300

    coreções

--- src/components/LadingPage/index.js
+++ src/components/LadingPage/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-export default function LadingPage() { 
-   return (
-    <div className='relative flex- h-200 w-100% flex justify-center'>
-        <img className='object-cover justify-end ' src='backgroung-home.png'/>
-        <p className='text-lg mb-4 absolute'>Vivant uma experiência única a cada gole.</p>
-    </div>
-   ) 
+export default function LadingPage() {
+    return (
+
+        <div className="relative h-screen flex flex-col justify-center md:px-16 lg:px-24">
+            <img className="absolute pl-75 brightness-65" src="backgroung-home.png" />
+
+            <div className="relative z-10 max-w-lg">
+                <p className="text-lg md:text-xl">
+                    Viva uma experiência única a cada gole.
+                </p>
+                <h1 className="text-4xl md:text-6xl font-bold mt-4 w-200">
+                    Desperte seus sentidos
+                </h1>
+                <button className="mt-6 px-6 py-3 bg-red-950 text-white text-lg rounded">
+                    Conheça nossa seleção de vinhos!
+                </button>
+            </div>
+
+        </div>
+    )
 }
